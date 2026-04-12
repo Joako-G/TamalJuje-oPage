399530b52afc69a756c4a00d91aab4ed23b35a8d
Joako-G <gallardojoako@gmail.com>
2026-04-11 23:31:49 -0300

    Eliminar estilos innecesarios de bootstrap

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -3,7 +3,7 @@ import { MenuList } from "../components/features/menu/MenuList/MenuList";
 
 export function MenuPage() {
     return (
-        <div className="container-fluid d-flex flex-column pb-5 col-11 justify-content-center mt-5 gap-4">
+        <div className="container-fluid d-flex flex-column pb-5 justify-content-center mt-5 gap-4">
             <MenuHeader />
             <MenuList />
         </div>
